6c13349715177701bb63bfe3a905fc1339b3e4c4
leonardo.carvalho <leo.carvalholkp@gmail.com>
2023-01-06 23:33:59 -0300

    test: use case register

---
+++ src/application/usecases/register-user-on-mailing-list/register-user-on-mailing-list.spec.ts
@@ -0,0 +1,13 @@
+describe("Regiter User on mailing list use case", () => {
+  it.skip("Should add user with complete data in mailing list", async () => {
+    //   const users: UserData[] = []
+    //   const userRepository: UserRepository = inMemoryUserRepository()
+    //   const registerUserOnMailingList = registerUserOnMailingList(userRepository)
+    //   const name = "any name"
+    //   const email = "any@email.com"
+    //   const response = await registerUserOnMailingList.execute({ name, email })
+    //   const user = await userRepository.findByEmail(email)
+    //   expect(user.name).toBe(name)
+    //
+  })
+})
