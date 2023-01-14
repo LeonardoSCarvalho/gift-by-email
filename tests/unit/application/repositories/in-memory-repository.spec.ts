390b564dd98e3d52fad2bb60a2a38377a5c6b9fa
leonardo.carvalho <leo.carvalholkp@gmail.com>
2023-01-14 16:16:34 -0300

    test: find user and check its existence

--- tests/unit/application/repositories/in-memory-repository.spec.ts
+++ tests/unit/application/repositories/in-memory-repository.spec.ts
@@ -8,4 +8,13 @@ describe("In memory User repository", () => {
     const user = await useRepository.findUserByEmail("any@email.com")
     expect(user).toBeNull()
   })
+  it("Should return if it is found in the repository", async () => {
+    const users: UserData[] = []
+    const name = "any_name"
+    const email = "any_email"
+    const userRepository = new InMemoryUserRepository(users)
+    await userRepository.add({ name, email })
+    const user = await userRepository.findUserByEmail(email)
+    expect(user?.name).toEqual(name)
+  })
 })
