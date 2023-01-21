d8f2578df609f14876e314a2b46a0357c161b8b0
leonardo.carvalho <leo.carvalholkp@gmail.com>
2023-01-21 20:25:11 -0300

    test: not accept domain part with more than 255 caracters

--- tests/unit/entities/email.spec.ts
+++ tests/unit/entities/email.spec.ts
@@ -13,4 +13,8 @@ describe("Email  validator", () => {
     const email = "a".repeat(65) + "@mail.com"
     expect(Email.validade(email)).toBeFalsy()
   })
+  it("Should not acccept domain part with more than 255 caracters", () => {
+    const email = "aby@mail." + "a".repeat(250) + ".com"
+    expect(Email.validade(email)).toBeFalsy()
+  })
 })
