e5d538eeba26af1f3e4f417f664e06ea5c620b18
leonardo.carvalho <leo.carvalholkp@gmail.com>
2023-02-26 14:24:19 -0300

    feat: create object value name

---
+++ src/domain/entities/name.ts
@@ -0,0 +1,15 @@
+import { Either, left, right } from "@/shared/either"
+import { InvalidNameError } from "../errors"
+
+export class Name {
+  private constructor(private readonly name: string) {}
+  static create(name: string): Either<InvalidNameError, Name> {
+    if (!Name.validate(name)) return left(new InvalidNameError())
+    return right(new Name(name))
+  }
+  static validate(name: string) {
+    if (!name) return false
+    if (name.trim().length < 2) return false
+    return true
+  }
+}
