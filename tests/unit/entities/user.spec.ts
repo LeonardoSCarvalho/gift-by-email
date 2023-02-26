f4cb57554e2151c24f12e03ef008cc4d5f0bb50f
leonardo.carvalho <leo.carvalholkp@gmail.com>
2023-02-26 14:30:07 -0300

    test: verify not to create user name with too few characters

--- tests/unit/entities/user.spec.ts
+++ tests/unit/entities/user.spec.ts
@@ -1,5 +1,6 @@
 import { User } from "@/domain/entities/user"
 import { InvalidEmailError } from "@/domain/errors/invalid-email-error"
+import { InvalidNameError } from "@/domain/errors/invalid-name-error"
 import { left } from "@/shared/either"
 
 describe("User domain entity ", () => {
@@ -9,5 +10,11 @@ describe("User domain entity ", () => {
     expect(error).toEqual(left(new InvalidEmailError()))
     expect(error.value).toBeInstanceOf(InvalidEmailError)
     expect(error.isLeft()).toBeTruthy()
+  })
+
+  it("Should not create user with invalid name (too few characters)", () => {
+    const invalidName = "o     "
+    const error = User.create({ name: invalidName, email: "any@email.com" })
+    expect(error).toEqual(left(new InvalidNameError()))
   })
 })
