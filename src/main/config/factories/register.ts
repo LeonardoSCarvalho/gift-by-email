548202b1172f92c319aa3a5949b312b3a3d6e74a
leonardo.carvalho <leo.carvalholkp@gmail.com>
2023-04-05 01:15:18 -0300

    feat: register user controller factory

---
+++ src/main/config/factories/register.ts
@@ -0,0 +1,11 @@
+import { RegisterUserOnMailingList } from "@/application/usecases/register-user-on-mailing-list/register-user-on-mailing-list"
+import { RegisterUserController } from "@/infra/controllers/register-user-controller"
+import { InMemoryUserRepository } from "@/infra/repositories/in-memory-user-repository"
+
+export const makeRegisterUserController = (): RegisterUserController => {
+  const inMemoryUserRepository = new InMemoryUserRepository([])
+  const registerUserOnMailingList = new RegisterUserOnMailingList(
+    inMemoryUserRepository
+  )
+  return new RegisterUserController(registerUserOnMailingList)
+}
