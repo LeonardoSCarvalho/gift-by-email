4f125c1d8e36c0ce95090601268ba6d069b27577
leonardo.carvalho <leo.carvalholkp@gmail.com>
2023-04-05 22:44:21 -0300

    feat: config router

--- src/main/config/adapters/express-route-adapter.ts
+++ src/main/config/adapters/express-route-adapter.ts
@@ -2,12 +2,13 @@ import { HttpRequest, HttpResponse } from "@/infra/controllers/ports"
 import { RegisterUserController } from "@/infra/controllers/register-user-controller"
 import { Request, Response } from "express"
 
-export const adaptroute = (controller: RegisterUserController) => {
+export const adaptRoute = (controller: RegisterUserController) => {
   return async (req: Request, res: Response) => {
     const httpRequest: HttpRequest = {
       body: req.body,
     }
     const httpResponse: HttpResponse = await controller.handle(httpRequest)
+    console.log(httpResponse.statusCode)
     res.status(httpResponse.statusCode).json(httpResponse.body)
   }
 }
--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,7 +1,9 @@
 import setupMiddleware from "@/main/config/middleware"
+import setupRoutes from "@/main/config/routes"
 import express from "express"
 
 const app = express()
 setupMiddleware(app)
+setupRoutes(app)
 
 export default app
